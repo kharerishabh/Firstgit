3531a9832db292915128e3e198565220d0d636aa
kharerishabh <114811133+kharerishabh@users.noreply.github.com>
2022-10-04 12:27:25 +0530

    deleteandEditfunctionality

---
+++ deleteandEditFunctionality/edit.js
@@ -0,0 +1,63 @@
+//Creating the edit button next to delete button
+
+// to grab the element
+let form = document.getElementById('addForm')
+let itemList = document.getElementById('items')
+
+//Form submit event
+form.addEventListener('submit', addItem)
+
+//Delete item using event listner
+itemList.addEventListener('click', removeItem);
+
+//Add item
+function addItem(e){
+    e.preventDefault()
+
+    //to check 
+   // console.log(1);
+
+   //get Input value
+   let newItem = document.getElementById('item').value
+
+   //create new li element
+   let li = document.createElement('li')
+
+    //Add class
+    li.className = 'list-group-item';
+    //console.log(li); //To check
+
+    //add text node with input value
+    li.appendChild(document.createTextNode(newItem))
+
+    //creating the delete button element
+    let deleteBtn = document.createElement('button');
+    let editBtn = document.createElement('button')
+
+    //Add classes to delete button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    editBtn.className = 'btn btn-danger btn-sm float-right edit';
+
+    //Add or appendtext node
+    deleteBtn.appendChild(document.createTextNode('X'));
+    editBtn.appendChild(document.createTextNode('edit'));
+    
+    //Append button on li
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+
+    //Append li to list
+    itemList.appendChild(li);
+}
+
+//Remove item
+function removeItem(e){
+    //console.log(1)//if you click the any item any where then it will count the click
+    if(e.target.classList.contains('delete')){
+        //console.log(1);//but this will only target the delete button
+    if(confirm('Are you sure?')){
+        let li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+    }
+}
